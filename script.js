7873becde1e22b961fbba407fc598bab8c431c6c
Daniel <danielbaowen@gmail.com>
2024-06-19 01:36:55 -0500

    fix several edge cases and bugs with calculation functionality and correctly display to console, save trailing 0's after decimal edge case

--- script.js
+++ script.js
@@ -13,18 +13,15 @@
 let display = 0;
 const MAX_DISPLAY = 9999999999;
 
-
-function getEquation() {
-    
-}
-
 function getInput() {
     let a = new Array();
     let c = new Array();
     let b = undefined;
     let negativeA = false;
     let negativeC = false;
+    const out = document.getElementById('console');
     const input = document.querySelectorAll('button');
+    out.innerText = 0;
     let focusOnA = true;
     input.forEach((button) => {
         button.addEventListener('click', (e) => {
@@ -34,15 +31,21 @@ function getInput() {
                     if (e.target.innerText == "=") {
                         if (!focusOnA) { // regular operation; click =
                                         // reset a, keep b and c
-                            let result = operate(a, b, c, negativeA, negativeC);
-                            a = result.toString().split('');
-                            if (result < 0) {
-                                a.shift();
-                                negativeA = true;
-                            } else
-                                negativeA = false;
                             
-                            focusOnA = true;
+                            if (c.length != 0) {
+                                let result = operate(a, b, c, negativeA, negativeC);
+                                a = result.toString().split('');
+                                if (result < 0) {
+                                    a.shift();
+                                    negativeA = true;
+                                } else
+                                    negativeA = false;
+                                
+                                focusOnA = true;
+
+                                display = arrayToNumber(a, negativeA);
+                                out.innerText = display;
+                            }
                         } else {        // if chained "="
                             let result = operate(a, b, c, negativeA, negativeC);
                             a = result.toString().split('');
@@ -51,29 +54,43 @@ function getInput() {
                                 negativeA = true;
                             } else
                                 negativeA = false;
-                        }
-                        
-                    } else {
 
-                        if (!focusOnA){ // not first time; click any other operation
-                                        // operate, set a, reset b and c
-                            let result = operate(a, b, c, negativeA, negativeC);
-                            a = result.toString().split('');
-                            if (result < 0) {
-                                a.shift();
-                                negativeA = true;
-                            } else
-                                negativeA = false;
-                            b = undefined;
+                            display = arrayToNumber(a, negativeA);
+                            out.innerText = display;
+                        }
+                    
+                        
+                    } else {    // any operation not =
+                        if (c.length != 0) { // if c exists
+                            if (!focusOnA){ // not first time; click any other operation
+                                            // operate, set a, reset b and c
+                                let result = operate(a, b, c, negativeA, negativeC);
+                                a = result.toString().split('');
+                                if (result < 0) {
+                                    a.shift();
+                                    negativeA = true;
+                                } else
+                                    negativeA = false;
+                            } 
+                            b = e.target.innerText;
+                            focusOnA = false;
                             c = [];
-                        } 
-                        b = e.target.innerText;
-                        focusOnA = false;
-                        c = [];
+
+                            display = arrayToNumber(a, negativeA);
+                            out.innerText = display; 
+                        } else if (b != e.target.innerText) { // if b changes operation
+                            b = e.target.innerText;
+                            focusOnA = false;
+                            c = [];
+                            display = arrayToNumber(a, negativeA);
+                            out.innerText = display;
+                        }
                     }
                     negativeC = false;
                     //first time, focusOnA: true to false
-                    //after, focusOnA: false to false                    
+                    //after, focusOnA: false to false 
+                    
+                     
                 } 
             } else if (e.target.className === 'gray') {
                 if (e.target.innerText === 'AC') {
@@ -83,6 +100,9 @@ function getInput() {
                     b = undefined;
                     negativeA = false;
                     negativeC = false;
+
+                    display = arrayToNumber(a, negativeA);
+                    out.innerText = display; 
                 } else if (e.target.innerText === '%') {
                     if (a.length > 0 && focusOnA) {
                         focusOnA = true;
@@ -93,6 +113,9 @@ function getInput() {
                         let index = a.indexOf('.');
                         a.splice(index, 1);
                         a.splice(index - 2, 0, '.');
+
+                        display = clean(arrayToNumber(a, negativeA));
+                        out.innerText = display; 
                     } else if (c.length > 0 && !focusOnA) {
                         // could implement c as a percentage of a
                         // rather than c * .01
@@ -104,42 +127,77 @@ function getInput() {
                         let index = c.indexOf('.');
                         c.splice(index, 1);
                         c.splice(index - 2, 0, '.');
+
+                        display = clean(arrayToNumber(c, negativeC));
+                        out.innerText = display; 
                     }
                 } else {
-                    if (focusOnA) // toggle sign
+                    if (focusOnA) {// toggle sign
                         negativeA = !negativeA;
-                    else 
+                        
+                        display = clean(arrayToNumber(a, negativeA));
+                        out.innerText = display; 
+                    }else {
                         negativeC = !negativeC;
+                        
+                        display = clean(arrayToNumber(c, negativeC));
+                        out.innerText = display; 
+                    }
                 }
-            } else if (focusOnA == true) {
-                //add edge cases for 0 and .
-                a.push(e.target.innerText);
-            } else {
-                //add edge cases for 0 and .
-                c.push(e.target.innerText);
+            } else if (focusOnA == true && a.length < 10) {
+                //DONE add edge cases for 0 and . 
+                //add edge case for overriding previous operation
+                //DONE add edge case for overflow
+                if (b != undefined && c.length != 0) {
+                    a = [];
+                    b = undefined;
+                    c = [];
+                    a.push(e.target.innerText);
+
+                    display = arrayToNumber(a, negativeA);
+                    out.innerText = display;
+                }else if (!a.includes('.') || !(e.target.innerText == '.')) {
+                    a.push(e.target.innerText);
+
+                    display = arrayToNumber(a, negativeA);
+                    out.innerText = display;
+                }
+                 
+            } else if (focusOnA == false && c.length < 10) {
+                //add edge cases for 0's and .
+                if (!c.includes('.') || !(e.target.innerText == '.')){
+                    c.push(e.target.innerText);
+
+                    display = arrayToNumber(c, negativeC);
+                    out.innerText = display; 
+                }
             }
-            display = a;
+            
             console.log('a: ' + a);
             console.log('b: ' + b);
             console.log('c: ' + c);
-            console.log('a: ' + arrayToNumber(a, negativeA));
-            console.log('b: ' + b);
-            console.log('c: ' + arrayToNumber(c, negativeC));
+            // console.log('a: ' + arrayToNumber(a, negativeA));
+            // console.log('b: ' + b);
+            // console.log('c: ' + arrayToNumber(c, negativeC));
         });
     }); 
 }
 
 function arrayToNumber(a, isNegative) { 
-    if (a === undefined || a.length == 0) return 0;
+    if (a === undefined || a.length == 0) return 0; // account for if number is just '.'
+    if (Number(a.join('')) == NaN) return "ERROR";
+    if (a.join('') == '.') return (isNegative) ? '-0.' : '0.';
+    if (a[a.length - 1] == '.') return Number(a.join('')) * ((isNegative) ? -1 : 1) + '.';
+    
     return Number(a.join('')) * ((isNegative) ? -1 : 1);
 }
 
 function operate(inputA, b, inputC, negativeA, negativeC) {
     a = arrayToNumber(inputA, negativeA);
     c = arrayToNumber(inputC, negativeC);
-    if (b === '/'){
+    if (b === '&#247'){
         return div(a, c);
-    } else if (b === 'x'){
+    } else if (b === '&#215;'){
         return mult(a, c);
     } else if (b === '-'){
         return min(a, c);
@@ -150,17 +208,11 @@ function operate(inputA, b, inputC, negativeA, negativeC) {
     return a;
 }
 
-function displayConsole() {
-    getEquation();
-    display = operate();
-}
+
 
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 addEventListener("DOMContentLoaded", () => {
-    const out = document.getElementById('console');
-    displayConsole();
     getInput();
-    out.innerText = display;
 });
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -185,7 +237,7 @@ function min(a, b){
 function clean(a) {
     a = Math.round(Number(a) * 10000000000)/10000000000; // Number() probably sets to scientific notation after certain
                                                             // amount of decimal places. Find new way to typecast 
-    if (a > MAX_DISPLAY) return MAX_DISPLAY;
+    if (a > MAX_DISPLAY) return NaN;
     if (Math.round(a) == a) return a;
     let lhs = Math.round(a).toString().length;
     console.log(lhs);
